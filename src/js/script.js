a9f78f2d485116af5fd4a3259b518b906d1645ea
hiropino1515 <hiropino1515@gmail.com>
2024-01-15 23:38:46 +0900

    フォント、カラーなど環境設定、共通button、header途中まで

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+  // ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass('is-active');
+      $("body").removeClass('is-active');
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $(".js-hamburger").addClass('is-active');
+      $("body").addClass('is-active');
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
 
 });
